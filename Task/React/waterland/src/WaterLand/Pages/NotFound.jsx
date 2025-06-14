7f74302aa574496c6b99882952f256a1f8ef64d3
karan-7006 <kharvarkaran21@gmail.com>
2025-06-14 15:21:41 +0530

    file name

--- Task/React/waterland/src/WaterLand/Pages/NotFound.jsx
+++ Task/React/waterland/src/WaterLand/Pages/NotFound.jsx
@@ -5,7 +5,7 @@ function NotFound() {
   return (
     <div>
       {/* 404 Start */}
-      <div className="container-fluid bg-light py-5">
+      <div className="container-fluid py-5">
         <div className="container py-5 text-center">
           <div className="row justify-content-center">
             <div className="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
